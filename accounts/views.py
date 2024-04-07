3404d0f5c816b37cbd63d307737a6ea80d78d999
masoud <aarvinb@gmail.com>
2024-04-08 02:47:57 +0330

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from accounts.forms import UserForm
-from accounts.models import User
+from accounts.models import User, UserProfile
+from vendor.forms import VendorForm
 
 
 # Create your views here.
@@ -37,3 +38,36 @@ def registerUser(request):
         'errors': form.errors,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES) #  request.FILES is for pdf or image reciving
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,
+                                            username=username, email=email, password=password
+                                            )
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been registered successfully! please wait for approval.')
+            return redirect('registerVendor')
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
