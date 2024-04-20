ed92b912d1bd75016c16e73fff9e026273977664
masoud <aarvinb@gmail.com>
2024-04-20 23:44:17 +0330

    forgot password features

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from FoodOnline_main.local_settings import secret_key, debug, db_name, db_user, db_pass, db_host, db_port
+from FoodOnline_main.local_settings import secret_key, debug, db_name, db_user, db_pass, db_host, db_port, \
+    email_address, email_pass, email_name
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#  Email configration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = email_address
+EMAIL_HOST_PASSWORD = email_pass
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = email_name
